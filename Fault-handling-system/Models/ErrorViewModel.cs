a9b564364949b89c3f2e3fb35931aca684034096
Maciej Chyrc <222112@student.pwr.edu.pl>
2018-03-10 18:30:31 +0100

    Created asp.net project.

---
+++ Fault-handling-system/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fault_handling_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
