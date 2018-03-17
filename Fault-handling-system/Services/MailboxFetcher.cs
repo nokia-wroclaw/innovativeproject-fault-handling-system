24d21da7e8ea57f964f5b07146f43b320ce344f6
Michał Szymański <smiszym@gmail.com>
2018-03-17 15:34:59 +0100

    Add mailbox fetcher interface and class

---
+++ Fault-handling-system/Services/MailboxFetcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Fault_handling_system.Services
+{
+    public class MailboxFetcher : IMailboxFetcher
+    {
+        private readonly ILogger<MailboxFetcher> _logger;
+        private readonly string _host;
+        private readonly int _port;
+        private readonly bool _useSSL;
+        private readonly string _username;
+        private readonly string _password;
+
+        public MailboxFetcher(ILogger<MailboxFetcher> logger,
+            string host, int port, bool useSSL, string username, string password)
+        {
+            _logger = logger;
+            _host = host;
+            _port = port;
+            _useSSL = useSSL;
+            _username = username;
+            _password = password;
+            _logger.LogInformation("Constructed MailboxFetcher");
+        }
+
+        public Task FetchMailbox()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
