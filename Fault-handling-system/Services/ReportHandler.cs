a6fae4b475eff8c4ef255949e008c906583e6093
Michał Szymański <smiszym@gmail.com>
2018-03-17 15:33:00 +0100

    Add report handler interface and class skeleton

---
+++ Fault-handling-system/Services/ReportHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Fault_handling_system.Services
+{
+    public class ReportHandler : IReportHandler
+    {
+        private readonly ILogger<ReportHandler> _logger;
+
+        public ReportHandler(ILogger<ReportHandler> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("Constructed ReportHandler");
+        }
+
+        public Task HandleReportAsync(string sender, string subject, string message)
+        {
+            _logger.LogInformation("Handling report:\n" +
+                                   "  Sender: {0}\n" +
+                                   "  Subject: {1}",
+                                   sender,
+                                   subject);
+            return Task.CompletedTask;
+        }
+    }
+}
