ddfb45cc10339bfa4760ee3df07031a8e7e9d346
Michał Szymański <smiszym@gmail.com>
2018-03-21 08:39:33 +0100

    Rename IReportHandler to IReportParser

--- Fault-handling-system/Services/ReportParser.cs
+++ Fault-handling-system/Services/ReportParser.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Fault_handling_system.Models;
 
 namespace Fault_handling_system.Services
 {
-    public class ReportHandler : IReportHandler
+    public class ReportParser : IReportParser
     {
-        private readonly ILogger<ReportHandler> _logger;
+        private readonly ILogger<ReportParser> _logger;
 
-        public ReportHandler(ILogger<ReportHandler> logger)
+        public ReportParser(ILogger<ReportParser> logger)
         {
             _logger = logger;
             _logger.LogInformation("Constructed ReportHandler");
         }
 
-        public bool HandleReport(string sender, string subject, string message)
+        public Report ParseReport(string sender, string subject, string message)
         {
             _logger.LogInformation("Handling report:\n" +
                                    "  Sender: {0}\n" +
                                    "  Subject: {1}",
                                    sender,
                                    subject);
-            return false;
+            return null;
         }
     }
 }
