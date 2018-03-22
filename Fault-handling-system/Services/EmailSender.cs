fae60b4686c7b983906564f128c4a9e4e89ac67b
Michał Szymański <smiszym@gmail.com>
2018-03-22 12:40:00 +0100

    Implement EmailSender with System.Net.Mail.SmtpClient

--- Fault-handling-system/Services/EmailSender.cs
+++ Fault-handling-system/Services/EmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Fault_handling_system.Services
@@ -11,6 +13,16 @@ namespace Fault_handling_system.Services
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
+            SmtpClient smtpClient = new SmtpClient("smtp.poczta.onet.pl", 465);
+            MailMessage msg = new MailMessage("pwr.fhs@onet.pl", email, subject, message);
+
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("pwr.fhs@onet.pl", "FaultHandlingSystem1");
+            msg.IsBodyHtml = true;
+
+            smtpClient.Send(msg);
             return Task.CompletedTask;
         }
     }
