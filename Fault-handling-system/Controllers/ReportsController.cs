fa8c1c181e2c86c7055f5da93ae9292f68a7b449
Piotr Sek <psek225932@e-science.pl>
2018-03-29 21:09:33 +0200

    download simply excel

--- Fault-handling-system/Controllers/ReportsController.cs
+++ Fault-handling-system/Controllers/ReportsController.cs
@@ -9,6 +9,9 @@ using Fault_handling_system.Data;
 using Fault_handling_system.Models;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using OfficeOpenXml;
 
 namespace Fault_handling_system.Controllers
 {
@@ -16,10 +19,12 @@ namespace Fault_handling_system.Controllers
     public class ReportsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public ReportsController(ApplicationDbContext context)
+        public ReportsController(IHostingEnvironment hostingEnvironment, ApplicationDbContext context)
         {
             _context = context;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         // GET: Reports
@@ -808,6 +813,53 @@ namespace Fault_handling_system.Controllers
         private bool ReportExists(int id)
         {
             return _context.Report.Any(e => e.Id == id);
+        }
+
+        [HttpPost]
+        [Route("Export")]
+        public FileContentResult Export(string test, IEnumerable<Report> lista)
+        {
+            byte[] bytes;
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = @"Report.xlsx";
+            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, sFileName);
+            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            if (file.Exists)
+            {
+                file.Delete();
+                file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            }
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                // add a new worksheet to the empty workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Employee");
+                //First add the headers
+                worksheet.Cells[1, 1].Value = "ID";
+                worksheet.Cells[1, 2].Value = "Name";
+                worksheet.Cells[1, 3].Value = "Gender";
+                worksheet.Cells[1, 4].Value = "Salary (in $)";
+
+                //Add values
+                worksheet.Cells["A2"].Value = 1000;
+                worksheet.Cells["B2"].Value = "Jon";
+                worksheet.Cells["C2"].Value = "M";
+                worksheet.Cells["D2"].Value = 5000;
+
+                worksheet.Cells["A3"].Value = 1001;
+                worksheet.Cells["B3"].Value = "Graham";
+                worksheet.Cells["C3"].Value = "M";
+                worksheet.Cells["D3"].Value = 10000;
+
+                worksheet.Cells["A4"].Value = 1002;
+                worksheet.Cells["B4"].Value = "Jenny";
+                worksheet.Cells["C4"].Value = "F";
+                worksheet.Cells["D4"].Value = 5000;
+
+                bytes = package.GetAsByteArray();
+
+            }
+
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
         }
     }
 }
