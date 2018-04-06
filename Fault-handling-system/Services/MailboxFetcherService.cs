6ab601acebb5ef3bd420c100ec106f38bb600380
Michał Szymański <smiszym@gmail.com>
2018-04-06 20:07:28 +0200

    Allow disabling mailbox fetching thread

--- Fault-handling-system/Services/MailboxFetcherService.cs
+++ Fault-handling-system/Services/MailboxFetcherService.cs
@@ -27,6 +27,11 @@ namespace Fault_handling_system.Services
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            if (!_settings.DoMailboxFetching) {
+                _logger.LogInformation("Not fetching mailbox, as requested in configuration");
+                return;
+            }
+
             stoppingToken.Register(() => 
                     _logger.LogDebug("Mail fetcher task is stopping..."));
 
