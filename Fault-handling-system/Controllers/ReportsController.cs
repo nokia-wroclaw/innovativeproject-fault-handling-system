5d223f65fcb2c4cbb60d86611fa7bfedd1bb3895
eszymczyk226044 <eszymczyk226044@e-science.pl>
2018-04-12 20:30:33 +0200

    Added assigning nokia coordinator to the report

--- Fault-handling-system/Controllers/ReportsController.cs
+++ Fault-handling-system/Controllers/ReportsController.cs
@@ -428,6 +428,25 @@ namespace Fault_handling_system.Controllers
             return View(report);
         }
 
+        [HttpPost]
+        [Authorize(Roles="Admin")]
+        public RedirectToActionResult Assign(int id)
+        {
+            string nsnCoordinator = Request.Form["NsnCoordinatorId"];
+            
+            if(nsnCoordinator != null)
+            {
+                var report = new Report();
+                report.Id = id;
+                report.NsnCoordinatorId = nsnCoordinator;
+                _context.Entry(report).Property("NsnCoordinatorId").IsModified = true;
+                _context.SaveChanges();
+            }
+            
+            return RedirectToAction("UserPage", "Account"); 
+        }
+
+
         // GET: Reports/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
