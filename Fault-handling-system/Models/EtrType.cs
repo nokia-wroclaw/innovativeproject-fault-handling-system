079fbf1e7f67b4f16e0ed0b8c1b3bf8e571c9303
Maciej Chyrc <222112@student.pwr.edu.pl>
2018-04-13 11:22:30 +0200

    Added xml comments.

--- Fault-handling-system/Models/EtrType.cs
+++ Fault-handling-system/Models/EtrType.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Fault_handling_system.Models
 {
-    public class EtrType
+	/// <summary>
+	/// Model representing EtrType db table in code.
+	/// </summary>
+	public class EtrType
     {
+		/// <value>Primary key.</value>
 		[Key]
 		public int Id { get; set; }
+		/// <value>String value of EtrType.</value>
 		[Required]
 		public string Type { get; set; }
     }
